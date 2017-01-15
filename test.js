d28af84de3b8366d5d845b0c685d1b825bdf5994
albNo273_EC2 <albireo0725.mikumiku@gmail.com>
2017-01-15 23:34:14 +0000

    fix cron date bug

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var f = require('./chk_update.js');
 let CronJob = require('cron').CronJob; 
 
-new CronJob('0 0,10,20,30,40,50 8-11,12-23 * * *', function() {
+new CronJob('0 0,10,20,30,40,50 0-8 * * *', function() {
     var ctime = Date();
     console.log(ctime);
     f.func();
