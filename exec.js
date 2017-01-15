2e7d1f14d7b6713af769a481a49bc661d43fcb43
Yuki Umeda <albireo0725.mikumiku@gmail.com>
2017-01-15 23:00:18 +0900

    test

--- exec.js
+++ exec.js
@@ -1,7 +1,7 @@
 var f = require('./chk_update.js');
 let cron = require('node-cron'); 
 
-cron.schedule('0,10,20,30,40,50 * * * * *', () => {
+cron.schedule('0 0,10,20,30,40,50 * * * *', () => {
     var ctime = Date();
     console.log(ctime);
     f.func();
