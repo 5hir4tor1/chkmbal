8d3b7f249250a250b1ac87218f8ebdde584bf46a
Yuki Umeda <albireo0725.mikumiku@gmail.com>
2017-01-16 22:47:02 +0900

    few updated.

--- exec.js
+++ exec.js
@@ -1,8 +1,11 @@
+/* 定期起動用(5分毎発火)) */
+
 var f = require('./chk_update.js');
 let CronJob = require('cron').CronJob; 
 
-new CronJob('0 0-55/5 8-23/1 * * *', function() {
+new CronJob('0 0-55/5 8-23 * * *', function() {
     var ctime = Date();
-    console.log(ctime);
+    console.log('\n=== ' + ctime + ' ===');
     f.func();
+    console.log();
 }, null, true, "Asia/Tokyo");
